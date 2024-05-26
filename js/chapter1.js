9c541b1b0c69e92c582f2ecff53360ef74abe9a3
HsingYu <cindychen20030212@gmail.com>
2024-05-27 04:16:13 +0800

    feat: chapter 1

--- js/chapter1.js
+++ js/chapter1.js
@@ -8,6 +8,13 @@ function next(id) {
     case 2:
       document.getElementById("second").style.display = "none";
       document.getElementById("third").style.display = "block";
+      document.getElementById("letter").style.display = "block";
       break;
+    case 3:
+      if(document.getElementById("answer").value == "華東門前"){
+        window.location.href = "./chapter2.html";
+      }else{
+        alert("答案錯誤");
+      }
   }
 }
